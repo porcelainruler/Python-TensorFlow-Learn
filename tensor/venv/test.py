f04b381f6b017acbd30d2b234409595dcfe6404f
Shubham <princeshubham22699@gmail.com>
2019-01-17 22:42:51 +0530

    TensorBoard

--- tensor/venv/test.py
+++ tensor/venv/test.py
@@ -4,4 +4,12 @@ import bs4 as bs4;
 node1 = tf.constant(3.0, tf.float32);
 node2 = tf.constant(4.0);
 
-print(node1, node2);
+c = node1*node2;
+
+sess = tf.Session ();
+
+File_writer = tf.summary.FileWriter('/home/shubham/PycharmProjects/tensor/venv/graph', sess.graph);
+
+print(sess.run(c));
+
+#print(node1, node2);
